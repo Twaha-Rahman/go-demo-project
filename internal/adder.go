5516340d9d45ee4cfb83969b9c18d82251ef4d63
Twaha Rahman <39026437+Twaha-Rahman@users.noreply.github.com>
2023-09-09 18:23:38 +0600

    add comment for the adder package

--- internal/adder.go
+++ internal/adder.go
@@ -1,5 +1,9 @@
+// A simple function to add two int numbers
 package adder
 
+// Add, adds two int numbers and return their sum as int
+//
+// It takes two parameters, `a` and `b`. Both of them are just int
 func Add(a int, b int) int {
 	return a + b
 }
