02952c00350d905a8dbe3ae6fa7b04f8f26bd360
Twaha Rahman <39026437+Twaha-Rahman@users.noreply.github.com>
2023-09-09 14:17:43 +0600

    add a simple package as an example

--- Makefile
+++ Makefile
@@ -12,6 +12,10 @@ vet: fmt
 	go vet ./...
 .PHONY:vet
 
+run: vet
+	go run cmd/goDemoProject/main.go
+.PHONY:run
+
 build: vet
 	go build -o bin/goDemoProject cmd/goDemoProject/main.go
 .PHONY:build
--- cmd/goDemoProject/main.go
+++ cmd/goDemoProject/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	adder "github.com/Twaha-Rahman/go-demo-project/internal"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	fmt.Println(adder.Add(69, 1))
 }
